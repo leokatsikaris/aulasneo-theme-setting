9579a9e0b1386b72f765fdb568b21e702cfa5c9b
leokatsikaris <leonelkatsikaris@gmail.com>
2022-11-30 13:22:09 -0300

    first dev commit

---
+++ react-theme-setting/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifythemesetting": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
